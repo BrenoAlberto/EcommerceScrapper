31c617dd9a69086650f7371581d94c91ca7b78e3
Breno Alberto <brenoaps435@gmail.com>
2023-01-13 20:27:51 -0300

    feat: get sorted lenovo laptops usecase factory

---
+++ src/main/factories/usecases/getSortedLenovoLaptops.ts
@@ -0,0 +1,12 @@
+import { GetSortedLenovoLaptops } from '@domain/usecases/getSortedLenovoLaptops'
+import { makeLaptopsPagePuppeteer } from '../pages/laptopsPagePuppeteer'
+import { makeProductPagePuppeteer } from '../pages/productPagePuppeteer'
+
+export const makeGetSortedLenovoLaptops = async (): Promise<GetSortedLenovoLaptops> => {
+  const laptopsPage = await makeLaptopsPagePuppeteer()
+  const productPage = await makeProductPagePuppeteer()
+  return new GetSortedLenovoLaptops(
+    laptopsPage,
+    productPage
+  )
+}
