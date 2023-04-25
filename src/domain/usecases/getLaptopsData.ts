8b0156e2ab3436cd2ce73e954feaa2ce3bcdd29b
Breno Alberto <brenoaps435@gmail.com>
2023-04-25 08:25:51 -0300

    feat+refactor: add concurrentTaskQueue function, replace getLaptopsData Promise.all with optimized parallel processing

--- src/domain/usecases/getLaptopsData.ts
+++ src/domain/usecases/getLaptopsData.ts
@@ -1,24 +1,16 @@
-import { Laptop } from '@/domain/models/laptop'
-import { IProductPage } from '@/domain/protocols/page'
+import { Laptop } from '@/domain/models/laptop';
+import { IProductPage } from '@/domain/protocols/page';
+import { concurrentTaskQueue } from '@/utils/promise';
 
 export class GetLaptopsData {
-  constructor (
-    private readonly productPage: IProductPage
-  ) { }
+  constructor(private readonly productPage: IProductPage) {}
 
-  async execute (laptopsURIs: string[]): Promise<Laptop.Model[]> {
-    const concurrency = 20
-    const laptopsData: Laptop.Model[] = []
-    for (let i = 0; i < laptopsURIs.length; i += concurrency) {
-      console.log(`Getting data from ${i} to ${i + concurrency} of ${laptopsURIs.length} laptops`)
-      const laptopsURIsChunk = laptopsURIs.slice(i, i + concurrency)
-      const laptopsDataChunk = await Promise.all(
-        laptopsURIsChunk.map(async uri => {
-          return this.productPage.getLaptopData(uri)
-        })
-      )
-      laptopsData.push(...laptopsDataChunk)
-    }
-    return laptopsData
+  async execute(laptopsURIs: string[]): Promise<Laptop.Model[]> {
+    const concurrency = 20;
+    const tasks = laptopsURIs.map(
+      (uri) => async () => await this.productPage.getLaptopData(uri)
+    );
+    const laptopsData = await concurrentTaskQueue(tasks, concurrency);
+    return laptopsData;
   }
 }
