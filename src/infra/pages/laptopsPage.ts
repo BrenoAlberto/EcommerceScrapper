e06173187290846ac5e1a8b1030abbee8f2aa55f
Breno Alberto <brenoaps435@gmail.com>
2023-04-25 08:55:59 -0300

    refactor: use Promise.all on getLaptopURIs use case

--- src/infra/pages/laptopsPage.ts
+++ src/infra/pages/laptopsPage.ts
@@ -1,35 +1,41 @@
-import { IBrowser } from '@/domain/protocols/browser'
-import { ILaptopListPage } from '@/domain/protocols/page'
+import { IBrowser } from '@/domain/protocols/browser';
+import { ILaptopListPage } from '@/domain/protocols/page';
 
 export class LaptopListPage implements ILaptopListPage {
   private readonly elementsXPathSelectors = {
     productsContainers: '//div[contains(@class, "thumbnail")]',
-    productTitle: '//div[@class="caption"]//a[@class="title"]'
+    productTitle: '//div[@class="caption"]//a[@class="title"]',
+  };
+
+  constructor(private readonly browser: IBrowser) {}
+
+  public async getLaptopURIs(pageURL: string, titleFilter: string): Promise<string[]> {
+    const page = await this.browser.newPage();
+    await page.goto(pageURL);
+
+    const productsContainers = await page.getElementsByXPath(this.elementsXPathSelectors.productsContainers);
+
+    const laptopURIsPromises = productsContainers.map((productContainer) =>
+      this.extractLaptopURI(productContainer, titleFilter)
+    );
+
+    const laptopsURIs = await Promise.all(laptopURIsPromises);
+    const filteredLaptopURIs = laptopsURIs.filter((uri) => uri !== null) as string[];
+
+    await page.close();
+    return filteredLaptopURIs;
   }
 
-  constructor (private readonly browser: IBrowser) { }
+  private async extractLaptopURI(productContainer: any, titleFilter: string): Promise<string | null> {
+    const title = await productContainer.getElementByXPath(this.elementsXPathSelectors.productTitle);
+    if (!title) return null;
 
-  public async getLaptopURIs (pageURL: string, titleFilter: string): Promise<string[]> {
-    const page = await this.browser.newPage()
-    await page.goto(pageURL)
+    const titleText = await title.getText();
+    if (!titleText || (titleFilter && !titleText.includes(titleFilter))) return null;
 
-    const productsContainers = await page.getElementsByXPath(this.elementsXPathSelectors.productsContainers)
-    const laptopsURIs: string[] = []
+    const uri = await title.getHref();
+    if (!uri) return null;
 
-    for (const productContainer of productsContainers) {
-      const title = await productContainer.getElementByXPath(this.elementsXPathSelectors.productTitle)
-      if (!title) continue
-      const titleText = await title.getText()
-      if (!titleText) continue
-      const uri = await title.getHref()
-      if (!uri) continue
-      if (titleFilter && !titleText.includes(titleFilter)) {
-        continue
-      }
-      laptopsURIs.push(uri)
-    }
-
-    await page.close()
-    return laptopsURIs
+    return uri;
   }
 }
